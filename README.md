c78a9937f3bb06cb080ec2dec82f7a0f06aa7dce
Emmanuel Harel <emmanuel.harel@gmail.com>
2021-05-20 11:15:51 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Kata babystepstimer
+Origine : https://github.com/dtanzer/babystepstimer/tree/master/Java  
+## Pre requis :
+- Java 11
+
+
---
+++ src/test/java/BabystepsTimerTest.java
@@ -0,0 +1,12 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BabystepsTimerTest {
+
+    @Test
+    public void initTest() {
+        assertThat(true).isEqualTo(true);
+    }
+
+}
